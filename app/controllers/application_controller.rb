3f7ef52d62c770dc10583d2257597d2163da3b52
Fabiane Carrijo <fabianecarrijo21@gmail.com>
2023-11-07 20:56:38 -0300

    ajustes em template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :authenticate_user!,  unless: :devise_controller?
   # skip_before_filter :verify_authenticity_token
   # layout :layout_by_resource
   #
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :interesse_alunos
   resources :agenda_alunos
